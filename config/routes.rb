516047beab91a3d600e54b0040b533f72f71986d
Abhishe Kumar <abhishekkr1993@gmail.com>
2018-08-03 11:18:38 +0530

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,30 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
 
+  get 'questions\index'
+
+  get 'questions\edit'
+
+  get 'questions\show'
+
+  get 'questions\new'
+
+  get 'answers\index'
+
+  get 'answers\edit'
+
+  get 'answers\show'
+
+  get 'answers\new'
+
+  get 'users\index'
+
+  get 'users\edit'
+
+  get 'users\show'
+
+  get 'users\new'
+  
   root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
